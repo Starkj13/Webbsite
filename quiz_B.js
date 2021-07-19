081c86aa2de03903f45298c99129f967947bd8a6
Starkj13 <87191869+Starkj13@users.noreply.github.com>
2021-07-19 17:10:05 +0200

    Add files via upload

---
+++ quiz_B.js
@@ -0,0 +1,160 @@
+// pos is position of where the user in the test or which question they're up to
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
+var questions = [
+  {
+      question: "Vem är Jesus?",
+      a: "1: En profet",
+      b: "X: Guds son",
+      c: "2: En evangelist",
+      answer: "B"
+    },
+  {
+      question: "Vilken blomma är inte gul?",
+      a: "1: Solros",
+      b: "X: Smörblomma",
+      c: "2: Förgätmigej",
+      answer: "C"
+    },
+  {
+      question: "Hur många spelkort finns det i en kortlek?",
+      a: "1: 52",
+      b: "X: 60",
+      c: "2: 62",
+      answer: "A"
+    },
+  {
+      question: "Vilka färgen behöver man blanda för att få färgen grön?",
+      a: "1: Blå + gul",
+      b: "X: Gul + röd",
+      c: "2: Röd + blå",
+      answer: "A"
+    },
+    {
+        question: "Vilket djur är långben?",
+        a: "1: Ko",
+        b: "X: Hund",
+        c: "2: Mus",
+        answer: "B"
+      },
+      {
+        question: "Vilka var Adam och Eva?",
+        a: "1: Jesus lärjungar",
+        b: "X: Sara och Abrahams barn",
+        c: "2: De första människorna på jorden",
+        answer: "C"
+      },
+      {
+        question: "Vilket träslag tillverkas glasspinnar av?",
+        a: "1: Bok",
+        b: "X: Björk",
+        c: "2: Al",
+        answer: "A"
+      },
+      {
+        question: "Arendal är namnet på vad för något?",
+        a: "1: Modiga Meridas pilbåge",
+        b: "X: Ariels mamma ",
+        c: "2: Elsa och Annas rike",
+        answer: "C"
+      },
+      {
+        question: "Vilken typ av fisk är Nemo från hitta Nemo?",
+        a: "1: Bläckfisk",
+        b: "X: Clownfisk",
+        c: "2: Guldfisk",
+        answer: "B"
+      },
+      {
+        question: "Vem var Michael Jackson?",
+        a: "1: En skådespelare",
+        b: "X: En sångare",
+        c: "2: En modell",
+        answer: "B"
+      },
+      {
+        question: "Vem var Goliat i bibelberättelsen om David och Goliat?",
+        a: "1: En hemsk jätte",
+        b: "X: En liten pojke",
+        c: "2: En kung",
+        answer: "A"
+      },
+      {
+        question: "Vad heter Grisens barn?",
+        a: "1: Lamm",
+        b: "X: Föl",
+        c: "2: Kulting",
+        answer: "C"
+      },
+  ];
+// this get function is short for the getElementById function  
+function get(x){
+  return document.getElementById(x);
+}
+// this function renders a question for display on the page
+function renderQuestion(){
+  test = get("test");
+  if(pos >= questions.length){
+    test.innerHTML = "<h2>Du fick "+correct+" av "+questions.length+" frågor rätt</h2>"
+    get("test_status").innerHTML = "Tipspromenad klar, Bra jobbat";
+    // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
+  }
+  get("test_status").innerHTML = "Fråga "+(pos+1)+" av "+questions.length;
+  
+  question = questions[pos].question;
+  chA = questions[pos].a;
+  chB = questions[pos].b;
+  chC = questions[pos].c;
+  // display the question
+  test.innerHTML = "<h3>"+question+"</h3>";
+  // display the answer options
+  // the += appends to the data we started on the line above
+ 
+  test.innerHTML += "<label> <input type='radio' id=radio1 name='choices' value='A'> "+chA+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' id=radio2 name='choices' value='B'> "+chB+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' id=radio3 name='choices' value='C'> "+chC+"</label><br><br>";
+  test.innerHTML += "<button onclick='checkAnswer()'>Svara</button>";
+}
+
+function checkAnswer(){
+  // use getElementsByName because we have an array which it will loop through
+  choices = document.getElementsByName("choices");
+  for(var i=0; i<choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
+    }
+  }
+  // checks if answer matches the correct choice
+  if(choice == questions[pos].answer){
+    //each time there is a correct answer this value increases
+    correct++;
+  }
+  // changes position of which character user is on
+  pos++;
+  // then the renderQuestion function runs again to go to next question
+  renderQuestion();
+}
+// Add event listener to call renderQuestion on page load event
+window.addEventListener("load", renderQuestion);
+
+
+// font size
+
+var radio, yPos;
+function yScroll(){
+radio = document.getElementById('radio1');
+yPos = window.pageYOffset;
+if(yPos > 120){
+    header.style.height = "36px";
+    header.style.paddingTop = "0px";
+    header.style.background = "#005A31";
+} else {
+    header.style.height = "120px";
+    header.style.paddingTop = "10px";
+    header.style.background = "#A8CD1B";
+}
+}
---
+++ quiz_V.js
@@ -0,0 +1,147 @@
+// pos is position of where the user in the test or which question they're up to
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
+var questions = [
+  {
+      question: "Vem döptes i Husaby källa?",
+      a: "1: Magnus Ladulås",
+      b: "X: Olof skötkonung",
+      c: "2: Erik Eriksson",
+      answer: "B"
+    },
+  
+  {
+      question: "Vad var S:ta Brigida skyddshelgon över?",
+      a: "1: Barn, sjuka, djur och smeder",
+      b: "X: Äldre, sjuka, djur och bönder",
+      c: "2: Äldre, sjuka, djur och smeder",
+      answer: "A"
+    },    
+    {
+    
+    question: "Hur länge bodde Lasse i Berget i grottan?",
+    a: "1: 35 år",
+    b: "X: 32 år",
+    c: "2: 30 år",
+    answer: "C"
+
+  },
+  {
+      question: "Vilka upptäckte Flyhovs hällristningar?",
+      a: "1: Barn med förälder",
+      b: "X: Två pojkar",
+      c: "2: Två flickor",
+      answer: "B"
+    },
+    {
+        question: "Vem var byggnadsherre för biskopsborgen?",
+        a: "1: Brynolf Gerlaksson",
+        b: "X: Adam av Bremen",
+        c: "2: Stefan av Alvastra",
+        answer: "A"
+      },
+      {
+        question: "Vad hette Olof Skötkonungs dotter?",
+        a: "1: Ingegerd",
+        b: "X: Ingbritt",
+        c: "2: Ingmarie",
+        answer: "A"
+      },
+      {
+        question: "Hur många församlingar deltog med stenar till tusenårsaltaret?",
+        a: "1: 100 st",
+        b: "X: 200 st",
+        c: "2: 300 st",
+        answer: "C"
+      },
+      {
+        question: "Vad kallades Ingegerd som nunna?",
+        a: "1: Heliga Birgitta",
+        b: "X: Heliga Anna av Novgorod",
+        c: "2: Heliga Ragnhild av Tälje",
+        answer: "B"
+      },
+      {
+        question: "Vad har det tidigare varit i Pilgrimsgården och Hembygdsgården?",
+        a: "1: Skola",
+        b: "X: Fritids",
+        c: "2: Lekis",
+        answer: "A"
+      },
+      {
+        question: "Vad heter den kortaste vandringsleden i Husaby?",
+        a: "1: Prinsstigen",
+        b: "X: Drottningsstigen",
+        c: "2: Kungsstigen",
+        answer: "C"
+      },
+      {
+        question: "Vad heter den nya kyrkogården?",
+        a: "1: Sankta Anna",
+        b: "X: Sankta Helena",
+        c: "2: Sankta Brigida",
+        answer: "A"
+      },
+      {
+        question: "Vilken kunglighet har inte besökt Husaby källa?",
+        a: "1: Kronprinsessan Victoria",
+        b: "X: Gustaf VI Adolf",
+        c: "2: Oscar II",
+        answer: "B"
+      },
+  ];
+// this get function is short for the getElementById function  
+function get(x){
+  return document.getElementById(x);
+}
+// this function renders a question for display on the page
+function renderQuestion(){
+  test = get("test");
+  if(pos >= questions.length){
+    test.innerHTML = "<h2>Du fick "+correct+" av "+questions.length+" frågor rätt</h2>"
+    get("test_status").innerHTML = "Tipspromenad klar, Bra jobbat";
+    // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
+  }
+  get("test_status").innerHTML = "Fråga "+(pos+1)+" av "+questions.length;
+  
+  question = questions[pos].question;
+  chA = questions[pos].a;
+  chB = questions[pos].b;
+  chC = questions[pos].c;
+  // display the question
+  test.innerHTML = "<h3>"+question+"</h3>";
+  // display the answer options
+  // the += appends to the data we started on the line above
+ 
+  test.innerHTML += "<label> <input type='radio' id=radio1 name='choices' value='A'> "+chA+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' id=radio2 name='choices' value='B'> "+chB+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' id=radio3 name='choices' value='C'> "+chC+"</label><br><br>";
+  test.innerHTML += "<button onclick='checkAnswer()'>Svara</button>";
+}
+
+function checkAnswer(){
+  // use getElementsByName because we have an array which it will loop through
+  choices = document.getElementsByName("choices");
+  for(var i=0; i<choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
+    }
+  }
+  // checks if answer matches the correct choice
+  if(choice == questions[pos].answer){
+    //each time there is a correct answer this value increases
+    correct++;
+  }
+  // changes position of which character user is on
+  pos++;
+  // then the renderQuestion function runs again to go to next question
+  renderQuestion();
+}
+// Add event listener to call renderQuestion on page load event
+window.addEventListener("load", renderQuestion);
+
+
